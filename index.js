1af8e9be26e23dd6d58b7451a0bce07211726485
sameeradeel <75333044+sameeradeel@users.noreply.github.com>
2021-05-21 01:06:36 +0500

    First Push

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const http = require('http');
+const socket = require('socket.io');
+const cors = require('cors');
+const app = express();
+let port = process.env.PORT || 3000;
+
+app.use(express.static('public'));
+app.use(cors());
+//const server = app.listen(4000, ()=> console.log("Listening at http://localhost:4000"));
+const server = app.listen(port, function(){
+  console.log('listening on', port);
+});
+const io = socket(server);
+io.on('connection', socket =>{
+    console.log("Connected on Socket with ID: "+socket.id);
+    socket.on('chat-message', function(data){
+        console.log(data);
+        io.sockets.emit('chat-message', data);
+    })
+    socket.on('typing', username => {
+        console.log(username);
+        socket.broadcast.emit('typing', username);
+    })
+})
